31755c807664388a9febc08c037dc36dc7c3c7f7
KemuelRocha <kemuelsr@gmail.com>
2022-08-28 15:13:53 -0300

    atualização

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-function selectionSort(){
-    var vetorElementos = document.getElementById("vetor");
-    var vetorOriginal = vetorElementos.value;
-    var resultado = vetorOriginal.split(",");
+function selectionSort(vetor){
+    let menor;
+    let tmp;
+    for (let i = 0; i < vetor.length; i++){
+        menor = i;
+        for (let j = i + 1; j < vetor.length; j++){
+            if (vetor[j] < vetor[menor]){
+                menor = j;
+            }
+        }
+        tmp = vetor[i];
+        vetor[i] = vetor[menor];
+        vetor[menor] = tmp;
+    }
+    return vetor;
+}
 
-    console.log(resultado);
-
-    // var vetor = [vetororiginal];
-    // var menor;
-    // for (var i = 0; i < vetor.length; i++){
-    //     menor = i;
-    //     for (var j = i + 1; j < vetor.length; j++){
-    //         if (vetor[j] < vetor[menor]){
-    //             menor = j;
-    //         }
-    //     }
-    //     if (i != menor){
-    //         [vetor[i], vetor[menor]] = [vetor[menor], vetor [i]];
-    //     }   
-    // }
-    // var Ordenado = vetor;
-    // var elementoOrdenado = document.getElementById("vetorOrdenado");
-    // var vetorOrdenado  = "Vetor ordenado: " + Ordenado;
-    // elementoValorConvertido.innerHTML = vetorOrdenado;
+function ordenar(){
+    var vetorElementos = document.getElementById("vetor_ordenar").value;
+    var resultado = vetorElementos.split(",").map(Number);
+    var entrada = JSON.parse(resultado);
+    document.getElementById("vetor_ordenado").innerHTML = selectionSort(entrada);
 }
 
